173d630ba9b65fc518e00face25aba7f00ca6c94
Ivan <vanesiotanaev@mail.ru>
2022-07-17 22:22:49 +0300

    BubbleMethod fixed

--- 17072022/Program.cs
+++ 17072022/Program.cs
@@ -39,22 +39,25 @@ void MinMaxSortMethod(int[] array)
     Console.WriteLine();
 }
 
-void GlassSortMethod(int[] array)
+void BubbleSortMethod(int[] array)
 {
-    for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (array [i] < array [i+1])
+        for (int j = 0; j < array.Length - 1; j++)
         {
-            int temp  = array[i];
-            array[i] = array [i+1];
-            array[i+1] = temp;
+            if (array[j] < array [j+1])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
         }
     }
     string sortedarray = string.Join(" ", array);
-    Console.WriteLine($"Отсортированный массив (метод Glass): {sortedarray}");
+    Console.WriteLine($"Отсортированный массив (метод Bubble): {sortedarray}");
     Console.WriteLine();
 }
 
 int[] userarray = UserArrayMethod();
-MinMaxSortMethod(userarray);
-GlassSortMethod(userarray);
+//MinMaxSortMethod(userarray);
+BubbleSortMethod(userarray);
